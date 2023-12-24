9f0186f556b7deeb9bf4f7f5dfe16fb7d586bcd5
Gordy <gordeyzhizhin@gmail.com>
2023-12-24 16:25:57 +0800

    second-instance, open file with app; refactor cache path

--- main.js
+++ main.js
@@ -1,16 +1,69 @@
 const { app, ipcMain} = require('electron')
 const path = require('path')
 //const imageinfo = require('imageinfo')
+const util = require('util');
+const settings = require('./lib/settings');
 
 const db = require('./lib/db');
-const settings = require('./lib/settings');
 
 let win;
 
 const winFiles = require('./lib/window/files');
 app.whenReady().then(() => {
-    win = winFiles();
-})
+    const file = getSelectedFile(process.argv);
+    if (file) {
+        const dir = path.dirname(file);
+        const name = path.basename(file);
+        const [ files, dirs ] = winFiles.readFiles(dir);
+
+        const detail = require('./lib/window/detail');
+        detail(null, files, name);
+    } else {
+        win = winFiles.open();
+    }
+});
+
+if (app.requestSingleInstanceLock()) {
+    app.on('second-instance', (event, argv, workingDirectory, additionalData = null) => {
+
+        if (win) {
+            const file = getSelectedFile(argv);
+            if (file) {
+                const dir = path.dirname(file);
+                const name = path.basename(file);
+                const [ files, dirs ] = winFiles.readFiles(dir);
+
+                const detail = require('./lib/window/detail');
+                detail(win, files, name);
+            } else {
+                if (win.isMinimized()) {
+                    win.restore();
+                }
+                win.focus();
+            }
+        }
+
+        // const log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'a'});
+        // log_file.write(util.format(argv) + "\n");
+        // log_file.write(util.format(additionalData) + "\n");
+        // log_file.close();
+    },);
+} else {
+    app.quit();
+}
+
+function getSelectedFile(args) {
+    for (const arg of args) {
+        for (const type of settings.types) {
+            if (arg.endsWith(`.${type}`)) {
+                return arg;
+            }
+        }
+    }
+    return null;
+}
+
+
 
 app.on('window-all-closed', () => {
     db.close();
